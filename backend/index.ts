a668b53ecb3344894ff844542b64074fcbb58f9c
Tracy Mullen <tracykmullen@gmail.com>
2023-08-06 12:10:24 -0700

    add express

--- backend/index.ts
+++ backend/index.ts
@@ -1,18 +1,12 @@
+import express from "express";
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
+const app = express();
 
-async function main() {
+app.get("/users", async (req: any, res: any) => {
   const allUsers = await prisma.user.findMany();
-  console.log(allUsers);
-}
+  res.json(allUsers);
+});
 
-main()
-  .then(async () => {
-    await prisma.$disconnect();
-  })
-  .catch(async (e) => {
-    console.error(e);
-    await prisma.$disconnect();
-    process.exit(1);
-  });
+const server = app.listen(3000);
